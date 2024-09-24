046a371719c7d2faf47e3ea8a924baa84dfd54a0
[sama durani] <[duranisama@gmail.com]>
2024-09-24 19:15:51 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -18,6 +18,7 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -52,6 +53,26 @@ api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+@app.route('/login', methods=['POST'])
+def post():
+    data = request.get_json()
+    user = User.query.filter(User.username == data['username']).first()
+    session['user_id'] = user.id
+
+    return user.to_dict(), 200
+
+@app.route('/logout', methods=['DELETE'])
+def delete():
+    session['user_id'] = None
+    return {}, 204
+
+@app.route('/check_session')
+def get():
+    user = User.query.filter(User.id == session.get('user_id')).first()
+    if user is not None:
+        return user.to_dict(), 200
+    
+    return {}, 401
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
